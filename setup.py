7b1494cad44425be0877ef69233a2083d1a76ded
diegoisla <diego.isla@outlook.com>
2021-12-15 18:42:31 +0100

    Modify requirements in setup

--- setup.py
+++ setup.py
@@ -2,18 +2,29 @@ from distutils.core import setup
 setup(
   name = 'blackbeard2109',         
   packages = ['blackbeard2109'],   
-  version = '0.1',    
+  version = '0.2',    
   license='MIT',        
   description = '''This library is designed for people who need to optimize time in an agile way with an ease of understanding and could 
   solve the main projects you may have in Data Science, starting with cleaning dataframe (including images), visualization and machine learning.''',   
   author = 'DS2109FULLTIME',                   
   author_email = 'datascience2109thebridge@gmail.com',     
   url = 'https://github.com/ds2109fulltime/BLACKBEARD',   
-  download_url = 'https://github.com/ds2109fulltime/BLACKBEARD/dist/blackbeard2109-0.1.tar.gz',    
+  download_url = 'https://github.com/ds2109fulltime/BLACKBEARD/dist/blackbeard2109-0.2.tar.gz',    
   keywords = ['machinelearning', 'scrum', 'datascience'],  
-  install_requires=[            
-          'tensorflow','xgboost','imageio','utllib','matplotlib','seaborn','scipy','requests','sys','collections',
-          'sklearn','pandas','numpy','cv2','plotly','folium','IPython','datetime','calendar','itertools','itertools'
+  install_requires=['pandas',
+                    'numpy',
+                    'opencv-python',
+                    'scikit-learn',
+                    'imageio',
+                    'folium',
+                    'plotly',
+                    'scipy',
+                    'IPython',
+                    'matplotlib',
+                    'seaborn',
+                    'tensorflow',
+                    'xgboost',
+                    'mljar-supervised'
       ],
   classifiers=[
     'Development Status :: 4 - Beta',      
