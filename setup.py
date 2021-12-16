87130cc951840aae18b9870a9c6494b10be591fa
diegoisla <diego.isla@outlook.com>
2021-12-16 15:59:43 +0100

    New Packaging method

--- setup.py
+++ setup.py
@@ -1,37 +1,24 @@
-from distutils.core import setup
-setup(
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
   name = 'blackbeard2109',         
-  packages = ['blackbeard2109'],   
-  version = '0.2',    
-  license='MIT',        
+  version = '0.2.1',    
+  author = 'The Bridge Data Science Team 2109',                   
+  author_email = 'datascience2109thebridge@gmail.com',
   description = '''This library is designed for people who need to optimize time in an agile way with an ease of understanding and could 
   solve the main projects you may have in Data Science, starting with cleaning dataframe (including images), visualization and machine learning.''',   
-  author = 'DS2109FULLTIME',                   
-  author_email = 'datascience2109thebridge@gmail.com',     
+  long_description=long_description,
+  long_description_content_type="text/markdown",
   url = 'https://github.com/ds2109fulltime/BLACKBEARD',   
-  download_url = 'https://github.com/ds2109fulltime/BLACKBEARD/dist/blackbeard2109-0.2.tar.gz',    
-  keywords = ['machinelearning', 'scrum', 'datascience'],  
-  install_requires=['pandas',
-                    'numpy',
-                    'opencv-python',
-                    'scikit-learn',
-                    'imageio',
-                    'folium',
-                    'plotly',
-                    'scipy',
-                    'IPython',
-                    'matplotlib',
-                    'seaborn',
-                    'tensorflow',
-                    'xgboost',
-                    'mljar-supervised'
-      ],
   classifiers=[
-    'Development Status :: 4 - Beta',      
-    'Intended Audience :: Developers',      
-    'Topic :: Software Development :: Build Tools',
-    'License :: OSI Approved :: MIT License',   
-    'Programming Language :: Python :: 3.7',      
-    'Programming Language :: Python :: 3.8',
+    "Programming Language :: Python :: 3",
+    "License :: OSI Approved :: MIT License",
+    "Operating System :: OS Independent",
   ],
+  package_dir={"": "src"},
+  packages=setuptools.find_packages(where="src"),
+  python_requires=">=3.6",
 )
